2c5786371f117312032344aa3318b23b249383ad
Munanga <jackslaighter@gmail.com>
2018-09-22 15:46:22 -0500

    Added views and likes.(Exercise P63)

--- tango_project/rango/admin.py
+++ tango_project/rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rango import models
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ( 'title', 'category', 'url')
+
 
 # Register your models here.
+admin.site.register(models.Category)
+admin.site.register(models.Page, PageAdmin)
+
+
